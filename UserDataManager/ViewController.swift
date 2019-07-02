b9008443d9e6a623b63d1ebeefedcafe3195cbde
Birapuram Kumar Reddy <kumar.reddy@swiggy.in>
2019-07-02 22:24:45 +0530

    Initial Commit

---
+++ UserDataManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDataManager
+//
+//  Created by kumar reddy on 02/07/19.
+//  Copyright © 2019 kumar reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
